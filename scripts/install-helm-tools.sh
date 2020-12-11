ab1301f40a660d82a86d13ee8899eeeb5abd426f
marzelwidmer <marzelwidmer@gmail.com>
2020-12-11 15:57:20 +0100

    Update prometheus install script

--- scripts/install-helm-tools.sh
+++ scripts/install-helm-tools.sh
@@ -3,7 +3,7 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 
-helm install prometheus prometheus-community/kube-prometheus-stack
+helm install prometheus --set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false  prometheus-community/kube-prometheus-stack
 helm install mongodb --set architecture=replicaset bitnami/mongodb
 helm install redis bitnami/redis
 
